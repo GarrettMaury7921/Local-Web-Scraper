e663bc299420e45446b33d8bea6deabfcac7d00f
Garrett Maury <90222973+GarrettMaury7921@users.noreply.github.com>
2023-11-29 18:24:16 -0500

    images + dir breaks with them tho

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ import re
 import spacy
 from bs4 import BeautifulSoup
 from bs4 import Comment
+from PIL import Image
+from PIL.ExifTags import TAGS
 
 # Load spaCy
 nlp = spacy.load("en_core_web_sm")
@@ -37,6 +39,8 @@ all_comments = []
 all_scripts = []
 all_tag_names = []
 
+all_image_data = []
+
 # For organizing all output into a single file
 main_path = os.getcwd() + "\\" + "a_script_output" + "\\" + "ALL_OUTPUT.txt"
 
@@ -55,105 +59,143 @@ def scrape_folders():
     global all_scripts
     global all_tag_names
 
-    list_of_files = os.listdir(path)
-    amount_of_files = len(list_of_files)
-    counter = 0
-    for files in list_of_files:
-        # For every file, increment the counter
-        counter += 1
+    if os.path.exists(path):
+        list_of_files = os.listdir(path)
+        amount_of_files = len(list_of_files)
+        counter = 0
+        for files in list_of_files:
+            # For every file, increment the counter
+            counter += 1
 
-        if debug_directory:
-            print(white_space + files)
+            if debug_directory:
+                print(white_space + files)
 
-        # Use recursion to go through more directories if there are more
-        if '_files' in files:
-            path = path + "\\" + files
-            white_space += white_space
-            scrape_folders()
-        else:
-            # Do Scraping
-            if '.html' in files:
-                # Try not to create output.txt file for one that already exists if ran once already
-                if 'output.txt' in files:
-                    continue
+            # Use recursion to go through more directories if there are more
+            if '_files' in files:
+                path = path + "\\" + files
+                white_space += white_space
+                scrape_folders()
+            else:
+                # Do Scraping
+                if '.html' in files:
+                    # Try not to create output.txt file for one that already exists if ran once already
+                    if 'output.txt' in files:
+                        continue
 
-                # Change dir
-                directory = os.getcwd() + "\\" + path + "\\" + files
+                    # Change dir
+                    directory = os.path.join(path, files)
 
-                with open(directory, 'r', encoding='utf-8') as file:
-                    html = file.read()
+                    if os.path.exists(directory):
+                        with open(directory, 'r', encoding='utf-8') as file:
+                            html = file.read()
 
-                soup = BeautifulSoup(html, 'html.parser')
+                    soup = BeautifulSoup(html, 'html.parser')
 
-                # Get all text in HTML
-                text = ''.join(soup.stripped_strings)
+                    # Get all text in HTML
+                    text = ''.join(soup.stripped_strings)
 
-                # Get all comments / scripts / tag names / links / phone numbers
-                # / people name (attempting to) in the HTML / descriptions
-                comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-                scripts = soup.find_all('script')
-                tag_names = [tag.name for tag in soup.find_all()]
-                links = soup.find_all('a', href=True)
-                descriptions = soup.find('meta', {'name': 'description'})
-                description = descriptions.get('content') if descriptions else None
+                    # Get all comments / scripts / tag names / links / phone numbers
+                    # / people name (attempting to) in the HTML / descriptions
+                    comments = soup.find_all(string=lambda text: isinstance(text, Comment))
+                    scripts = soup.find_all('script')
+                    tag_names = [tag.name for tag in soup.find_all()]
+                    links = soup.find_all('a', href=True)
+                    descriptions = soup.find('meta', {'name': 'description'})
+                    description = descriptions.get('content') if descriptions else None
 
-                # Common phone number formats
-                phone_number_pattern = re.compile(r'\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b')
-                phone_numbers = re.findall(phone_number_pattern, html)
+                    # Common phone number formats
+                    phone_number_pattern = re.compile(r'\b\d{3}[-.\s]?\d{3}[-.\s]?\d{4}\b')
+                    phone_numbers = re.findall(phone_number_pattern, html)
 
-                # Get people names with spaCy
-                text_processed = nlp(text)
-                names = [ent.text for ent in text_processed.ents if ent.label_ == 'PERSON']
+                    # Get people names with spaCy
+                    text_processed = nlp(text)
+                    names = [ent.text for ent in text_processed.ents if ent.label_ == 'PERSON']
 
-                # Get Email addresses with regex
-                emails = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
-                email_addresses = re.findall(emails, html)
+                    # Get Email addresses with regex
+                    emails = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
+                    email_addresses = re.findall(emails, html)
 
-                # Spit out all of the comments/scripts into a separate txt file
-                output_file_path = directory + "_output.txt"
+                    # Spit out all of the comments/scripts into a separate txt file
+                    output_file_path = directory + "_output.txt"
 
-                # Write to files
-                with open(output_file_path, 'w', encoding='utf-8') as output_file:
-                    # IN ORDER OF MOST IMPORTANT
-                    for number in phone_numbers:
-                        output_file.write(number + '\n')
-                        all_phone_numbers.append(number)
+                    # Write to files
+                    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+                        # IN ORDER OF MOST IMPORTANT
+                        for number in phone_numbers:
+                            output_file.write(number + '\n')
+                            all_phone_numbers.append(number)
 
-                    for email_address in email_addresses:
-                        output_file.write(email_address + '\n')
-                        all_emails.append(email_address)
+                        for email_address in email_addresses:
+                            output_file.write(email_address + '\n')
+                            all_emails.append(email_address)
 
-                    for name in names:
-                        output_file.write(name + '\n')
-                        all_names.append(name)
+                        for name in names:
+                            output_file.write(name + '\n')
+                            all_names.append(name)
 
-                    for link in links:
-                        output_file.write(link['href'] + '\n')
-                        all_links.append(link['href'])
+                        for link in links:
+                            output_file.write(link['href'] + '\n')
+                            all_links.append(link['href'])
 
-                    # Description
-                    output_file.write(str(description))
-                    all_descs.append(str(description))
+                        # Description
+                        output_file.write(str(description))
+                        all_descs.append(str(description))
 
-                    for comment in comments:
-                        output_file.write(str(comment) + '\n')
-                        all_comments.append(str(comment))
+                        for comment in comments:
+                            output_file.write(str(comment) + '\n')
+                            all_comments.append(str(comment))
 
-                    for script in scripts:
-                        output_file.write(str(script) + '\n')
-                        all_scripts.append(str(script))
+                        for script in scripts:
+                            output_file.write(str(script) + '\n')
+                            all_scripts.append(str(script))
 
-                    for tag_name in tag_names:
-                        output_file.write(tag_name + '\n')
-                        all_tag_names.append(tag_name)
+                        for tag_name in tag_names:
+                            output_file.write(tag_name + '\n')
+                            all_tag_names.append(tag_name)
+
+                # If it's an image file, find EXIF
+                if '.jpg' in files or '.JPEG' in files or '.JPG' in files or '.png' in files or \
+                        '.PNG' in files or '.gif' in files or '.bmp' in files or '.WebP' in files:
+
+                    if 'output.txt' in files:
+                        continue
+
+                    # Change dir
+                    directory = os.path.join(path, files)
+
+                    image = Image.open(directory)
+                    exifdata = image.getexif()
+
+                    # Spit out all of the comments/scripts into a separate txt file
+                    output_file_path = directory + "_output.txt"
+
+                    # Write to files
+                    with open(output_file_path, 'w', encoding='utf-8') as output_file:
+                        # Get the Metadata
+                        all_image_data.append("FILE: " + directory)
+
+                        for tag_id in exifdata:
+                            tag_name = TAGS.get(tag_id, tag_id)
+                            value = exifdata.get(tag_id)
+
+                            output_file.write(f"{tag_name:25}: {value}" + '\n')
+                            all_image_data.append(f"{tag_name:25}: {value}" + '\n')
 
 
-            if counter == amount_of_files:
-                white_space = white_space[:-5]
-                path = original_path
+                if counter == amount_of_files:
+                    white_space = white_space[:-5]
+                    path = original_path
 
 
-# Goes through all of the directories and checks inside them for files
+# Remove duplicates in the lists
+def remove_duplicates(big_list):
+    unique_list = []
+    for item in big_list:
+        if item not in unique_list:
+            unique_list.append(item)
+
+    return unique_list
+
 
 # For printing files to verify directories
 debug_directory = True
@@ -172,7 +214,20 @@ for file in local_files:
         original_path = path
         scrape_folders()
 
-# Sort all the output in the final file
+
+# Remove Duplicates
+if debug_directory:
+    print('Removing Duplicates!')
+all_phone_numbers = remove_duplicates(all_phone_numbers)
+all_emails = remove_duplicates(all_emails)
+all_names = remove_duplicates(all_names)
+all_links = remove_duplicates(all_links)
+all_tag_names = remove_duplicates(all_tag_names)
+
+
+# Sort all the output in the final file and write to it
+if debug_directory:
+    print('Writing to output file!')
 with open(main_path, 'a', encoding='utf-8') as main_output_file:
     for numbers in all_phone_numbers:
         main_output_file.write(numbers + '\n')
@@ -190,3 +245,8 @@ with open(main_path, 'a', encoding='utf-8') as main_output_file:
         main_output_file.write(script + '\n')
     for tags in all_tag_names:
         main_output_file.write(tags + '\n')
+    for data in all_image_data:
+        main_output_file.write(data + '\n')
+
+if debug_directory:
+    print('Done!')
